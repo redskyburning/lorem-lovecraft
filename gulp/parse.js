71f0147c14aa42a8ad15b7e9e0348e34ecde70b2
Ben Webb <ben@redskyburning.com>
2017-09-03 14:58:02 -0400

    basic manifest, refactoring

--- gulp/parse.js
+++ gulp/parse.js
@@ -1,13 +1,17 @@
 'use strict';
 
-let path     = require('path');
-let gulp     = require('gulp');
-let conf     = require('./conf');
-let $        = require('gulp-load-plugins')();
-let through2 = require('through2');
+let path       = require('path');
+let gulp       = require('gulp');
+let conf       = require('./conf');
+let $          = require('gulp-load-plugins')();
+let through2   = require('through2');
+let changeCase = require('change-case');
 
 function convertToJson() {
-	return through2.obj(function(file, encoding, callback) {
+	let manifest      = [];
+	let referenceFile = null;
+
+	function transformFunction(file, encoding, callback) {
 		const targetString = '***target***';
 		let textArray      = [];
 
@@ -28,16 +32,38 @@ function convertToJson() {
 			});
 
 		file.contents = new Buffer(JSON.stringify(textArray));
+
+		// Change to json ext and normalize name
+		let pathObj  = path.parse(file.path);
+		pathObj.base = changeCase.paramCase(pathObj.name) + '.json';
+		file.path    = path.format(pathObj);
+
+		// Push info to manifest and save ref file for later if needed
+		if (referenceFile === null) {
+			referenceFile = file.clone();
+		}
+		manifest.push(path.basename(file.path));
+
 		callback(null, file);
-	});
+	}
+
+	function flushFunction(callback) {
+		let pathObj            = path.parse(referenceFile.path);
+		pathObj.base           = 'manifest.json';
+		referenceFile.path     = path.format(pathObj);
+		referenceFile.contents = new Buffer(JSON.stringify(manifest));
+
+		this.push(referenceFile);
+		callback();
+	}
+
+	return through2.obj(transformFunction, flushFunction);
 }
 
 gulp.task('parse', [], function() {
 	return gulp.src([path.join(conf.paths.bookSrc, '/**/*.txt')])
 		.pipe(convertToJson())
-		.pipe($.rename({
-			extname: '.json'
-		}))
+		.pipe($.jsbeautifier())
 		.pipe($.debug())
 		.pipe(gulp.dest(conf.paths.bookOutput));
 });
