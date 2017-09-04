4521b5c0cbdb5dff66def7d7c8f2331d83d87fb0
Ben Webb <ben@redskyburning.com>
2017-09-03 23:46:51 -0400

    wip

--- src/app/controllers/random-book/random-book.controller.js
+++ src/app/controllers/random-book/random-book.controller.js
@@ -1,19 +1,31 @@
 export class RandomBookController {
-	constructor($log, bookService) {
+	constructor($log, $q, bookService) {
 		'ngInject';
 
 		this.$log        = $log;
+		this.$q          = $q;
 		this.bookService = bookService;
-		this.book = null;
+
+		this.book       = null;
 		this.paragraphs = [];
 
-		this.bookService.getRandomBook()
-			.then((book) => {
-				this.book = book;
-				this.paragraphs = this.book.getRandomParagraphSequence(8);
-			})
+		this.randomize()
 			.catch((error) => {
 				this.$log.error(error);
 			});
+	}
+
+	randomize() {
+		return this.$q((resolve,reject) => {
+			this.bookService.getRandomBook()
+				.then((book) => {
+					this.book       = book;
+					this.paragraphs = this.book.getRandomParagraphSequence(5);
+					resolve();
+				})
+				.catch((error) => {
+					reject(error);
+				});
+		});
 	}
 }
