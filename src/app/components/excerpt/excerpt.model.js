0c7e7fdbb752884dbb6fe959e291fa99eb15741b
Ben Webb <ben@redskyburning.com>
2017-09-07 21:11:47 -0400

    excerpt service and passage support

--- src/app/components/excerpt/excerpt.model.js
+++ src/app/components/excerpt/excerpt.model.js
@@ -1,5 +1,6 @@
 export class ExcerptModel {
-	constructor() {
-		this.foo = 'bar';
+
+	constructor(paragraphs = []) {
+		this.paragraphs = paragraphs;
 	}
 }
--- src/app/controllers/passage/passage.controller.js
+++ src/app/controllers/passage/passage.controller.js
@@ -7,6 +7,6 @@ export class PassageController {
     this.book           = book;
     this.excerptService = excerptService;
 
-    this.passage = this.excerptService.getExcerptFromBook(Number($stateParams.paragraphCount), (seed / 1000));
+    this.excerpt = this.excerptService.getExcerptFromBook(book,Number($stateParams.paragraphCount), (seed / 1000));
   }
 }
