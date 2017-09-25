4e8852c6a5878592bfbc5c9d88a60b042d0789bf
Ben Webb <ben@redskyburning.com>
2017-09-25 19:37:04 -0400

    toggle only on book routes

--- src/app/controllers/main/main.controller.js
+++ src/app/controllers/main/main.controller.js
@@ -1,15 +1,20 @@
 export class MainController {
-  constructor($log, $rootScope, sidebarStateService) {
+  constructor($log, $rootScope, $transitions, sidebarStateService) {
     'ngInject';
 
     this.$log                = $log;
     this.$rootScope          = $rootScope;
     this.sidebarStateService = sidebarStateService;
 
-    this.isSidebarOpen   = false;
+    this.isSidebarOpen = false;
+    this.showToggle    = false;
 
-    this.sidebarListener = $rootScope.$on(this.sidebarStateService.changeEventName, (event,isOpen) => {
+    this.sidebarListener = $rootScope.$on(this.sidebarStateService.changeEventName, (event, isOpen) => {
       this.isSidebarOpen = isOpen;
+    });
+
+    $transitions.onSuccess({},(transition) => {
+      this.showToggle = transition.to().name.includes('main.book');
     });
   }
 
